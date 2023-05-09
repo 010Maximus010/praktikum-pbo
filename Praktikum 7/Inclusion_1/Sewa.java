8b5ec315daa3b6412e1c2019df9bb9c157467bfe
MAFT <100400751+010Maximus010@users.noreply.github.com>
2023-05-09 19:39:39 +0700

    Update Sewa.java

--- Praktikum 7/Inclusion_1/Sewa.java
+++ Praktikum 7/Inclusion_1/Sewa.java
@@ -2,7 +2,7 @@
 //Penulis: Muhammad Afiat Yulianto
 //NIM: 24060121140141
 //LAB: PBO B2
-//Deskripsi: 
+//Deskripsi: Program untuk demo Polimorfisme Inclusion
 
 public class Sewa{
 	public static void main(String[] args){
